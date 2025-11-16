a5d60c12b5a03c100879d70d566d3e21450938cd
redeadeyed <ruslan.mamaiev@gmail.com>
2025-11-16 13:19:56 +0100

    First commit

---
+++ Topics/Increment and decrement/Increment, decrement, then double n/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // Read the integer input
+        int n = Integer.parseInt(reader.readLine());
+
+        // TODO: Increment n by 1
+
+        // TODO: Decrement n by 2
+
+        // TODO: Double the resulting value of n
+
+        // Print the final value of n
+        System.out.println(n);
+    }
+}
---
+++ Topics/Naming variables/From cryptic to crystal  Weather's facelift/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int t = 25;
+        int h = 60;
+        int w = 6;
+
+        // TODO: Refactor variable names and print the results
+    }
+}
